dd44c49b93af560ddba2925e36be8763b3054333
0length <fajarjunyor13@gmail.com>
2020-01-29 12:26:26 +0700

    create module menu access getter

---
+++ app/lib/utils/getAccess.ts
@@ -0,0 +1,22 @@
+import { prisma } from "../../../prisma/src/generated/prisma-client";
+import { getUserId } from "../utils/json-web-token/verifyer"
+
+
+export const getAccess = async (context: any)=>{
+    const email = getUserId(context) 
+    // const email = await getUserId(context).then((email:any)=>email).catch((err:any)=>err) 
+    const  user  = await prisma.user({email})
+    if(!user){
+        throw new Error('No such user found')
+    }
+    const { group } = user
+    if(!group){
+        throw new Error('No access for this user')
+    }
+    const Group = await prisma.group({name: group})
+    const access = Group?.access
+    if(!access){
+        throw new Error('Nothing to access for this user')
+    }
+    return access
+}
