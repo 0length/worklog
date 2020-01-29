8fd15c8d87320ed3b261ea54ec4352d6eb0545f4
0length <fajarjunyor13@gmail.com>
2020-01-29 12:20:48 +0700

    Revert "update  static asset"
    This reverts commit 7ceeb39d7aa0d160fec3e74baace24eb8edaf82e.

--- utils/copyStaticAssets.js
+++ utils/copyStaticAssets.js
@@ -2,14 +2,7 @@ const fs = require('fs-extra')
 
 try {
   // fs.copySync('src/public/favicon.ico', 'dist/public/favicon.ico')
-  //font-awesome
-  fs.copy('static_assets/plugins', 'dist/public/plugins', function (err) {
-    if (err){
-        console.log('An error occured while copying plugins.')
-        return console.error(err)
-    }
-    console.log('Copy completed!')
-  }); 
+
   console.log('######## static assets copy: OK ########')
 } catch (err) {
   console.error('######## static assets copy: ERROR ########', err.message)
