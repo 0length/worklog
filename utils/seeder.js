9af74c1e2d140dad40272d2c4b10f67bbb8dc4dc
0length <fajarjunyor13@gmail.com>
2020-01-31 16:17:11 +0700

    update seeder

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,7 +1,13 @@
  const {prisma} = require('../prisma/src/generated/prisma-client');
 
 const menuSeeder = async ({name, sequence})=>await prisma.createMenu({name, sequence});
-
 const groupSeeder = async ({name, access})=>await prisma.createGroup({name, access})
+
+const initGroup = [
+    {name:'SuperUser', access:"user:'crud', group:'crud', menu:'crud', work:'crud', post:'crud'}"},
+    {name:'Admin', access:"user:'r', group:'', menu:'r', work:'crud', post:'crud'}"},
+    {name:'Guest', access:"user:'', group:'', menu:'r', work:'crud', post:'crud'}"},
+]
+
 menuSeeder({name: "dashboard", sequence: 1})
-groupSeeder({name:'SuperUser', access:"user:'crud', group:'crud', menu:'crud', work:'crud', post:'crud'}"})
+initGroup.map((item, idx)=>groupSeeder(item))
