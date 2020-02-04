513147f8c8a922ae546a7f896d5779d5033e6cd2
0length <fajarjunyor13@gmail.com>
2020-02-04 10:13:18 +0700

    create menu seeder

--- utils/seeder.js
+++ utils/seeder.js
@@ -4,10 +4,20 @@ const menuSeeder = async ({name, sequence})=>await prisma.createMenu({name, sequ
 const groupSeeder = async ({name, access})=>await prisma.createGroup({name, access})
 
 const initGroup = [
-    {name:'SuperUser', access:"user:'crud', group:'crud', menu:'crud', work:'crud', post:'crud'}"},
-    {name:'Admin', access:"user:'r', group:'', menu:'r', work:'crud', post:'crud'}"},
-    {name:'Guest', access:"user:'', group:'', menu:'r', work:'crud', post:'crud'}"},
+    {name:'SuperUser', access:"dashboard:'r', user:'crud', group:'crud', menu:'crud', work:'crud', post:'crud'}"},
+    {name:'Admin', access:"dashboard:'r', user:'r', group:'', menu:'r', work:'crud', post:'crud'}"},
+    {name:'Guest', access:"dashboard:'r', user:'', group:'', menu:'r', work:'crud', post:'crud'}"},
 ]
 
-menuSeeder({name: "dashboard", sequence: 1})
+const initMemu = [
+    {name:'dashboard', sequence: 0},
+    {name:'work', parent_name: "dashboard", sequence: 0},
+    {name:'post', parent_name: "dashboard", sequence: 1},
+    {name:'comment', parent_name: "dashboard", sequence: 2},
+    {name:'user', sequence: 1},
+    {name:'group', sequence: 2},
+    {name:'menu', sequence: 3},
+]
+
+initMemu.map((item)=>menuSeeder(item))
 initGroup.map((item, idx)=>groupSeeder(item))
