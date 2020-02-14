2a5c87f67c662e3970c9c5009458d78e049ea196
0length <fajarjunyor13@gmail.com>
2020-02-14 19:50:51 +0700

    create toast reducer

---
+++ reducer/toast/index.ts
@@ -0,0 +1,18 @@
+import { toastType } from "./types";
+import { initMenu } from "../init";
+
+const toastReducer = (state = {data:[]}, action: any)=>{
+
+    switch (action.type){
+        case toastType.SET:
+        return {...state};
+        case toastType.SETED:
+        return {data : [...state.data, ...action.recieve]};
+        case toastType.CLEAR:
+        return {data:[]};
+        default:
+        return state;
+    }
+}
+
+export default toastReducer
---
+++ reducer/toast/types.ts
@@ -0,0 +1,14 @@
+const SET = 'SET';
+const SETED = 'SETED';
+const CLEAR = 'CLEAR';
+
+
+const toastType = {
+    SET,
+    SETED,
+    CLEAR
+}
+
+export { 
+    toastType
+}
