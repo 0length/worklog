5a07f97b0febac3b31982e878ec560ba41009d64
0length <fajarjunyor13@gmail.com>
2020-02-14 19:53:17 +0700

    create work desk and testing component

---
+++ view/admin/components/work-desk/component/Testing.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { pushToast } from '../../../../../reducer/toast/action';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+
+const Testing: React.FC<any> = (props)=>{
+  
+    return(<>
+        <button style={{zIndex: 10000, color: 'blue', position: 'absolute', left: '200px', top: '50px'}} onClick={ ()=>{
+          const newData={message: "Ini adalah uji coba tapi jangan di coba-coba.", timeOut: 5000, type: "info", created_at: new Date+""};
+          const newData2={message: "Ini adalah uji coba tapi jangan di coba-coba.", timeOut: 7000, type: "success", created_at: new Date+""};
+          const newData3={message: "Ini adalah uji coba tapi jangan di coba-coba.", timeOut: 3000, type: "warning", created_at: new Date+""};
+          const newData4={message: "Ini adalah uji coba tapi jangan di coba-coba.", timeOut: 2000, type: "danger", created_at: new Date+""};
+
+           props.pushToast([newData, newData2, newData3, newData4, newData, newData2, newData3, newData4, newData, newData2, newData3, newData4, newData, newData2, newData3, newData4,  newData, newData2, newData3, newData4]);
+        //   await setNotification([]);
+        // setTimeout(() => {
+        //   console.log(notification.indexOf(newData),notification.indexOf(newData2))
+        //   // setNotification(notification.splice(notification.indexOf(newData), 1))
+        //   // setNotification(notification.splice(notification.indexOf(newData2), 1))
+        //   // setNotification(notification.splice(notification.indexOf(newData3), 1))
+        //   // setNotification([]);
+        // }, 1);
+        }}>Push Toast</button>
+    </>)
+}
+const mapStateToProps = (state:any) => (state);
+
+const mapDispatchToProps = (dispatch:any) =>
+    bindActionCreators({
+      pushToast
+    }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Testing);
+
---
+++ view/admin/components/work-desk/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled,{ createGlobalStyle } from 'styled-components'
+import Testing from './component/Testing'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+const GlobalStyle = createGlobalStyle``
+const Wrapper = styled.div`
+    display: flex;
+    position: absolute;
+    left: 15vw;
+    top: 17.5vh;
+    background: #fff;
+    box-shadow: 0 0 40px 0 rgba(82,63,105,.1);
+    border-radius: 4px; 
+    width: 83vw;
+    height: 88vh;
+`
+interface HighOrderType {
+    [index: string]: JSX.Element
+}
+
+const HighOrder:  HighOrderType= {
+    'testing' : <Testing />
+}
+
+const WorkDesk: React.FC<any> = (props)=>{
+    const activeMenu = props.menu.active
+    return(<Wrapper>
+        {HighOrder[activeMenu]}
+    </Wrapper>)
+}
+const mapStateToProps = (state:any) => (state);
+
+const mapDispatchToProps = (dispatch:any) =>
+    bindActionCreators({
+    }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(WorkDesk);
