66ff2aecceb87be211a8208d001df0a6fe7a2c30
0length <fajarjunyor13@gmail.com>
2020-02-17 20:12:42 +0700

    const for local storage key dir

---
+++ lib/const/localStorageKeys.ts
@@ -0,0 +1,9 @@
+const auth_token: string = 'WORKLOG://User/auth_token'
+const username: string = 'WORKLOG://User/data/username'
+
+const localStorageKeys = {
+    auth_token,
+    username
+}
+
+export default localStorageKeys
