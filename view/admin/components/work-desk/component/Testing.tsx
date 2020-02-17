347b7bbc8d9c576c43329a951f06b59ee726c98b
0length <fajarjunyor13@gmail.com>
2020-02-17 20:15:42 +0700

    test image rendering

--- view/admin/components/work-desk/component/Testing.tsx
+++ view/admin/components/work-desk/component/Testing.tsx
@@ -7,6 +7,7 @@ import { bindActionCreators } from 'redux';
 const Testing: React.FC<any> = (props)=>{
   
     return(<>
+        <img src="https://awsimages.detik.net.id/community/media/visual/2019/12/12/029c0887-7cc3-4f96-b666-4f7dea48f937_169.jpeg?w=700&q=90" />
         <button style={{zIndex: 10000, color: 'blue', position: 'absolute', left: '200px', top: '50px'}} onClick={ ()=>{
           const newData={message: "Ini adalah uji coba tapi jangan di coba-coba.", timeOut: 5000, type: "info", created_at: new Date+""};
           const newData2={message: "Ini adalah uji coba tapi jangan di coba-coba.", timeOut: 7000, type: "success", created_at: new Date+""};
