419a773e5be1345c6a92162f67aef72064e700d2
0length <fajarjunyor13@gmail.com>
2020-02-18 10:46:42 +0700

    add user init

--- utils/seeder.js
+++ utils/seeder.js
@@ -2,13 +2,17 @@
 
 const menuSeeder = async ({name, parent_name, sequence})=>await prisma.createMenu({name, parent_name, sequence});
 const groupSeeder = async ({name, access})=>await prisma.createGroup({name, access})
+const userSeeder = async ({name, username, email, password})=>await prisma.createUser({name, username, email, password})
+
 
 const initGroup = [
     {name:'SuperUser', access:`{"dashboard":"r", "user":"crud", "group":"crud", "menu":"crud", "work":"crud", "post":"crud"}`},
     {name:'Admin', access:`{"dashboard":"r", "user":"r", "group":"", "menu":"r", "work":"crud", "post":"crud"}`},
     {name:'Guest', access:`{"dashboard":"r", "user":"", "group":"", "menu":"r", "work":"crud", "post":"crud"}`},
 ]
-
+const initUSer = [
+    {name: 'admin', username: 'admin', email: 'admin@this.site', password: 'ikisandiku'}
+]
 const initMemu = [
     {name:'dashboard', parent_name: "", sequence: 0},
     {name:'work', parent_name: "dashboard", sequence: 0},
@@ -21,4 +25,5 @@ const initMemu = [
 
 initMemu.map((item)=>menuSeeder(item))
 initGroup.map((item, idx)=>groupSeeder(item))
+initUSer.map((item, idx)=>userSeeder(item))
 console.log("done")
