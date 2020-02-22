e2f9921b305eef0c2c5a38021f241a78ac9a84a8
0length <fajarjunyor13@gmail.com>
2020-02-22 21:16:24 +0700

    modify something

--- view/admin/components/work-desk/component/Testing.tsx
+++ view/admin/components/work-desk/component/Testing.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { pushToast } from '../../../../../reducer/toast/action';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { ToastCard } from '../../element';
+import { ToastCard, Dropzone } from '../../element';
 
 
 const Testing: React.FC<any> = (props)=>{
   
     return(<>
-        <img src="https://awsimages.detik.net.id/community/media/visual/2019/12/12/029c0887-7cc3-4f96-b666-4f7dea48f937_169.jpeg?w=700&q=90" />
-        <button style={{zIndex: 10000, color: 'blue', position: 'absolute', left: '200px', top: '50px'}} onClick={ ()=>{
+        <button style={{zIndex: 10000, color: 'blue', left: '0px', top: '50px'}} onClick={ ()=>{
           const newData={message: "Ini adalah uji coba tapi jangan di coba-coba.", timeOut: 10000, type: "info", created_at: new Date+""};
           const newData2={message: "Ini adalah uji coba tapi jangan di coba-coba.", timeOut: 13000, type: "success", created_at: new Date+""};
           const newData3={message: "Ini adalah uji coba tapi jangan di coba-coba.", timeOut: 7000, type: "warning", created_at: new Date+""};
@@ -25,7 +24,7 @@ const Testing: React.FC<any> = (props)=>{
         //   // setNotification([]);
         // }, 1);
         }}>Push Toast</button>
-
+      
     </>)
 }
 const mapStateToProps = (state:any) => (state);
