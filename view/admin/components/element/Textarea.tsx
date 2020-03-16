253a2843c180957ba5adeed2e9d6b6abc0d8da31
0length <fajarjunyor13@gmail.com>
2020-03-16 12:23:52 +0700

    create textarea element

---
+++ view/admin/components/element/Textarea.tsx
@@ -0,0 +1,31 @@
+import Styled from 'styled-components'
+
+const Textarea = Styled(Styled.textarea`
+display: block;
+width: 100%;
+height: auto;
+padding: .65rem 1rem;
+font-size: 1rem;
+font-weight: 400;
+line-height: 1.5;
+color: #495057;
+background-color: #fff;
+background-clip: padding-box;
+border: 1px solid #e2e5ec;
+border-radius: 4px;
+transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-box-shadow .15s ease-in-out;
+
+`)`
+&:focus {
+    color: #495057;
+    background-color: #5fccff;
+    outline: 0;
+}
+
+&::placeholder {
+    color: #74788d;
+    opacity: 1;
+}
+`
+
+export default Textarea
