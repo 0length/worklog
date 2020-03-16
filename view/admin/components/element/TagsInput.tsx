fe36a7fc0c7e920ef6db60ee9af29bad8216495d
0length <fajarjunyor13@gmail.com>
2020-03-16 17:05:06 +0700

    update style and event

--- view/admin/components/element/TagsInput.tsx
+++ view/admin/components/element/TagsInput.tsx
@@ -27,10 +27,14 @@ transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-bo
 const TagsInput: React.FC<any> = ({valueGetter, className})=>{
     const [tags, setTags] = useState<string[]>([])
     const [dom, setdom] = useState<JSX.Element[]>([])
+    const [focus, setFocus] = useState<boolean>(false)
 
     const inputRef = useRef<any>()
-    const focus = ()=>{
+    const handleTofocus = ()=>{
         inputRef.current.focus()
+        console.log(inputRef.current.__proto__)
+        inputRef.current.onblur = ()=>setFocus(false)
+        setFocus(true)
     }
 
     const addTag = (text: string)=>{
@@ -40,9 +44,9 @@ const TagsInput: React.FC<any> = ({valueGetter, className})=>{
     }
 
     const pressMatch = (e: any)=>{
-        // console.log(e)
+        // console.log(e.key)
 
-        if((e.key === 'Enter' || e.key === 'Space')&& e.target.value && tags.indexOf(e.target.value) === -1){
+        if((e.key === 'Enter' || e.key === ' ')&& e.target.value && tags.indexOf(e.target.value) === -1){
             addTag(e.target.value)
             e.target.value = ""
         }
@@ -71,7 +75,7 @@ setdom(tags.map((item, index)=>{
 }, [tags])
 
 return (
-    <Span className={className} onClick={()=>focus()}>
+    <Span className={className} style={ focus?{border: '1px solid #A3C5FC'}:undefined} onClick={()=>handleTofocus()}>
         {
             dom
         }
