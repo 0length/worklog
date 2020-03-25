acee490f8a7c95ee8f96ea87aba5699d42e54a75
0length <fajarjunyor13@gmail.com>
2020-03-25 16:21:19 +0700

    create customhook for general graphql request

---
+++ lib/hook/useGrapher.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch  } from "react-redux"
+import { generalGraph, killGrapher } from '../../reducer/actions'
+
+export interface GrpaherPayload {
+    method: 'mutation'|'query'|'subscription'|''
+    doWhat: string
+    varIn?: string
+    varOut: string
+}
+
+export interface Grapher {
+payload: GrpaherPayload
+processId: string
+status: string
+data?: any
+}
+
+export interface GrapherState {
+    grapher: Grapher
+    setGrapher: (overrides: Partial<Grapher>)=>void
+}
+
+const useGrapher = (overrides?: Partial<Grapher>): GrapherState=>{
+    // const {payload, processId, status} = overrides
+    const defaultState: Grapher = {
+        payload: {
+            method: "",
+            varIn: '',
+            varOut: '',
+            doWhat: ''
+        },
+        processId: "",
+        status: "",
+        data: ''
+    }
+    const dispatch = useDispatch()
+    const [ grapher, setG ]  = useState<Grapher>(
+        {
+            // payload: {...defaultState.payload, ...overrides.payload},
+            // processId: overrides.processId||defaultState.processId,
+            // status: overrides.status||defaultState.status
+            ...defaultState,
+            ...overrides
+        }
+    )
+    const setGrapher = (newVal: Partial<Grapher>)=>setG({
+        ...grapher,
+        ...newVal
+    })
+    const globalGrapher = useSelector( (state: any) => state.grapher[ grapher.processId ] )
+    useEffect(()=>{
+        const { method, varIn, varOut, doWhat } = grapher.payload
+        if(grapher.status === 'submit'){
+            dispatch(
+                generalGraph(
+                    `${method}{
+                        ${doWhat}${varIn?'('+varIn+')':''}{
+                            ${varOut}
+                        }
+                    }`,
+                    grapher.processId
+                )
+            )
+            setGrapher({status: 'inProgress'})
+        }
+    }, [ grapher.status ])
+
+    useEffect(()=>{
+        if( globalGrapher){
+            setGrapher(
+                {
+                    ...grapher,
+                    status: globalGrapher.data?'finish':
+                            globalGrapher.error?'error: '+globalGrapher.error:
+                            globalGrapher.isLoading?'inProgress':'',
+                    data: globalGrapher.data?globalGrapher.data:''
+                }
+            )
+            dispatch( killGrapher( grapher.processId ) )
+        }
+     }, [ globalGrapher ])
+
+    return { grapher,  setGrapher }
+}
+
+export default useGrapher
